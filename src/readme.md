ca84d36f5496ff4fe34f55f5ec081402f852b83a
WhiteChilli <locmerea@gmail.com>
2023-02-23 13:23:14 +0200

    Added User unit tests method and constructors

---
+++ src/readme.md
@@ -0,0 +1,6 @@
+
+| Nr  | Test |  Status  |
+|:----|:----:|:--------:|
+| 1   | tba  |   tba    |
+| 2   | tba  |   tba    |
+| 3   | tba  |   tba    |
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,41 +1,36 @@
 package org.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 
 public class AppTest 
 {
 
     @Test
-    public void checkSum()
-    {
+    public void checkSum() {
         Calculator calculator = new Calculator();
         int sumResult = calculator.doSum(7, 6);
         assertEquals(13, sumResult);
     }
 
     @Test
-    public void checkSumNegativeNum()
-    {
+    public void checkSumNegativeNum() {
         Calculator calculator = new Calculator();
         int sumResult = calculator.doSum(-9, 8);
         assertEquals(-1, sumResult);
     }
 
     @Test
-    public void checkSumZero()
-    {
+    public void checkSumZero() {
         Calculator calculator = new Calculator();
         int sumResult = calculator.doSum(0, 0);
         assertEquals(0, sumResult);
     }
 
     @Test
-    public void checkDiff()
-    {
+    public void checkDiff() {
         Calculator calculator = new Calculator();
         int diffResult = calculator.doDiff(7, 6);
         assertEquals(1, diffResult);
@@ -49,12 +44,32 @@ public class AppTest
     }
 
     @Test
-    public void checkDiffZero()
-    {
+    public void checkDiffZero() {
         Calculator calculator = new Calculator();
         int diffResult = calculator.doDiff(0, 0);
         assertEquals(0, diffResult);
     }
+
+    @Test
+    public void checkNameWasUpdated() {
+        Calculator calculator = new Calculator();
+        User user = new User("Bob",355, 44);
+        user.changeName("Rob");
+        assertEquals("Rob", user.name);
+    }
+
+    @Test
+    public void checkConstructorWithData() {
+        User user = new User("Tiffany", 582, 33);
+        assertNotNull(user.name);
+    }
+
+    @Test
+    public void checkConstructorWithoutData() {
+        User user = new User();
+        assertNull(user.name);
+    }
+
 }
 
 
